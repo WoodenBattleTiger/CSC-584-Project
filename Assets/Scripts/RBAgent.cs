00974870f751eb4aafd9176b57a0c76b7d5a266c
Claire A <aguiar.claire@gmail.com>
2025-04-01 15:49:36 -0400

    draft boost placement function

--- Assets/Scripts/RBAgent.cs
+++ Assets/Scripts/RBAgent.cs
@@ -42,8 +42,88 @@ public class RBAgent : MonoBehaviour
         //place an obstacle tile
     }
 
-    public void PlaceBoost()
+    public (int,int) PlaceBoost(TileGrid grid, int sourceRow, int sourceCol, int goalRow, int goalCol, int boostsRemaining)
     {
-        //place an obstacle tile
+        //players get 3 boosts
+
+        //place first boost near character in the direction of the goal
+        int rowDis = sourceRow - goalRow;
+        int colDis = sourceCol - goalCol;
+
+        (int, int) tileLocation = (-1, -1);
+
+        if (boostsRemaining == 3)
+        {
+            //first boost placement
+            if (Mathf.Abs(rowDis) >= Mathf.Abs(colDis))
+            {
+                //the difference in row takes priority, place boost in row closer to goal
+                if (rowDis > 0)
+                {
+                    //goal is below source
+                    tileLocation = (sourceRow - 1, sourceCol);
+
+                }
+                else
+                {
+                    //goal is above source
+                    tileLocation = (sourceRow + 1, sourceCol);
+                }
+
+            }
+            else
+            {
+                //col difference takes priorty, place boost in col closer to goal
+                if (colDis > 0)
+                {
+                    //goal is to the left of source
+                    tileLocation = (sourceRow, sourceCol - 1);
+                }
+                else
+                {
+                    //goal is to the right source
+                    tileLocation = (sourceRow, sourceCol - 1);
+                }
+            }
+        }
+        else if (boostsRemaining == 2)
+        {
+            if (Mathf.Abs(rowDis) >= Mathf.Abs(colDis))
+            {
+                //the difference in row takes priority, place boost in row closer to character
+                if (rowDis > 0)
+                {
+                    //goal is below source
+                    tileLocation = (goalRow + 1, goalCol);
+
+                }
+                else
+                {
+                    //goal is above source
+                    tileLocation = (goalRow - 1, goalCol);
+                }
+
+            }
+            else
+            {
+                //col difference takes priorty, place boost in col closer to character
+                if (colDis > 0)
+                {
+                    //goal is to the left of source
+                    tileLocation = (goalRow, goalCol + 1);
+                }
+                else
+                {
+                    //goal is to the right source
+                    tileLocation = (goalRow, goalCol + 1);
+                }
+            }
+        }
+        //else
+        //{
+        //    
+        //}
+
+        return tileLocation;
     }
 }
